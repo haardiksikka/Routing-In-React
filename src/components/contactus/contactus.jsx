136285ab90c5be98de01b4ded4275480a17d1c61
haardiksikka <hsikka95@gmail.com>
2020-01-10 12:01:14 +0530

    updated contact us, and login module

--- src/components/contactus/contactus.jsx
+++ src/components/contactus/contactus.jsx
@@ -3,8 +3,15 @@ import React from "react";
 
 function template() {
   return (
-    <div className="contactus">
-      <h1>contactus</h1>
+    <div className="contactus text-center">
+      <h1>Contact Us</h1>
+      <br/>
+      <br/>
+      <p> <i> Mahindra Comviva </i> </p>
+      <p> Capital Cyberspace </p>
+      <p> Sector 59 </p>
+      <p> Gurugram </p>
+      <p> <b> Email to :</b> <a> Mahindra@chodu.com </a> </p>
     </div>
   );
 };
--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -6,22 +6,19 @@ function template() {
     <div className="projects">
       <h1>Login</h1>
       <form onSubmit={this.onSub}>
-        <table className="table ">
+        <table className="table" cellSpacing="10" cellPadding="10">
           <tr>
             <td>Username</td>
-            <td><input type="text" name="uname" onChange={this.onCh1} /> <br/></td>
-            <br/>
+            <td><input type="text" name="uname" onChange={this.onCh1} /> </td>
+           
           </tr>
           <tr>
             <td>Password</td>
             <td><input type="password" name="pwd" onChange={this.onCh2} />
-              <br />
-              <br />
             </td>
           </tr>
 
           <tr className="text-center">
-
             <td><input class="btn btn-danger" type="submit" value="Login" /> &nbsp;
                             <input class="btn btn-info" type="reset" value="Cancel" /></td>
           </tr>
